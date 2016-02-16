ddb9d7035c8bfb489ce293e80c2b9cb75e9373c5
Rafael Tavares Andrade <rafaeltavaresandrade@gmail.com>
2016-02-16 09:40:08 -0300

    regions

--- UnitSocial.W10/ViewModels/AdvertisingViewModel.cs
+++ UnitSocial.W10/ViewModels/AdvertisingViewModel.cs
@@ -6,7 +6,7 @@ namespace UnitSocial.ViewModels
 
         public AdvertisingViewModel()
         {
-            Title = "Advertising";            
+            Title = "Publicidade";            
         }        
     }
 }
